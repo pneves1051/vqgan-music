4b76b7035b623ba28a6cf49d70b595c0ea4e6536
pneves1051 <79472414+pneves1051@users.noreply.github.com>
2021-03-27 17:33:53 -0300

    Include WGAN-GP loss

--- models/losses.py
+++ models/losses.py
@@ -26,3 +26,23 @@ def mel_loss(output, real, n_fft=1024, hop_length=256, sample_rate=44100):
 def loss_fn(outputs, targets, top_codes, bottom_codes, beta=0.25):
   return torch.Tensor([mse_loss(outputs, targets), latent_loss(top_codes, beta), latent_loss(bottom_codes, beta)])
 
+# INCLUDE D OR G TO CALCULATE PREDS AND INTERPOLATION
+def WGAN_loss(d_real, d_fake, mode):
+  if mode = 'd':
+    d_loss = -(d_real.mean() - d_fake.mean())
+    # Gradient penalty
+    eps = torch.rand((self.b_size, 1, 1, 1)).repeat(1, *real.shape[1:]).to(self.device)
+    interp = (eps*real+ (1-eps)*fake).to(self.device)
+    d_interp = self.discriminator(interp, conditions)
+    gp = torch.autograd.grad(outputs=d_interp, inputs=interp,
+                              grad_outputs=torch.ones_like(d_interp),
+                              create_graph=True, retain_graph=True)[0]          
+    gp = gp.view(gp.shape[0], -1)
+    gp = ((gp.norm(2, dim=1) - 1)**2).mean()     
+
+    d_loss_gp = d_loss + 10*gp
+    return d_loss_gp
+  
+  elif mode == 'g':
+    g_loss = -d_fake.mean()   
+    return g_loss
